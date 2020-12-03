bc83a6c5c8f2bbe59d7d224131421f10fc618dff
Silke Husse <silke.husse@uni.kn>
2020-12-03 19:52:14 +0100

    reviewed version

--- playground/txt_fetcher.py
+++ playground/txt_fetcher.py
@@ -3,13 +3,18 @@ from urllib.request import urlopen
 from urllib.error import HTTPError
 import re
 
-class txt(object):
+class txtFetcher(object):
     """
-    docstring tba
+    docstring tbd
     """
     def __init__(self):
-        self.encoding_type = "utf-8"  # default encoding
-        self.flag = False
+        self.encoding = ["utf-8", "ISO-8859-1", "latin1"]
+        self.encoding_idx = 0 # default value
+        self.encoding_final = self.encoding[self.encoding_idx] # default value
+        self.flag_final = False
+        self.flag_encoding = False
+        self.lines_list = []
+        self.first_line = False
 
     def verify_url(self, url):
         """
@@ -17,88 +22,109 @@ class txt(object):
         """
         return "offenedaten-konstanz.de" in url
 
+    def get_file_ending(self, url):
+        """
+        extract file ending from an url
+        """
+        return url.split(".")[::-1][0]
+
+    def get_encoding(self, file):
+        """
+        extract encoding from first line if applicable
+        """
+        try:
+            first_line = file.readline().decode(self.encoding[self.encoding_idx])
+        except:
+            print("false encoding")
+            self.flag_encoding = True
+            if self.encoding_idx < len(self.encoding)-1:
+                self.encoding_idx += 1
+                self.get_encoding(file)
+            else:
+                print("no matching encoding found. please review")
+
+        if re.findall("encoding", first_line):
+            self.first_line = True
+            for i in re.finditer("encoding", first_line):
+                text = first_line[i.end():]
+                self.encoding_final = str(re.findall('"([^"]*)"', text)[0]).strip('[]')
+
+    def read_line(self, line):
+        """
+        read single line
+        """
+        decoded_line = line.decode(self.encoding_final)
+        for quoted_part in re.findall(r'\"(.*?)\"', decoded_line):  # replace 'space' in quotes with '@'
+            decoded_line = decoded_line.replace(quoted_part, quoted_part.replace(" ", "@"))
+        split_lines = decoded_line.split(' ')
+        for i in range(len(split_lines)):
+            for quoted_part in re.findall(r'\"(.*?)\"',
+                                          split_lines[i]):  # replace '@' in quotes with 'space'
+                split_lines[i] = split_lines[i].replace(quoted_part, quoted_part.replace("@", " "))
+        self.lines_list.append(split_lines)
+
     def get_data(self, url):
 
         try:
             file = urlopen(url)
         except HTTPError:
-            print("download failed")
-            self.flag = True
+            print("could not open url")
+            self.flag_final = True
 
-        if self.flag == False:
-
-            lines_list = []
-
+        if self.flag_final == False:
             try:
-                first_line = file.readline().decode(self.encoding_type)
-                if re.findall("encoding", first_line):
-                    for i in re.finditer("encoding", first_line):
-                        text = first_line[i.end():]
-                        self.encoding_type = str(re.findall('"([^"]*)"', text)).strip('[]')
-                else:
-                    for quoted_part in re.findall(r'\"(.+?)\"', first_line):
-                        first_line = first_line.replace(quoted_part, quoted_part.replace(" ", "@"))
-                    split_lines = first_line.split(' ')
-                    lines_list.append(split_lines)
-            except Exception as read_error:
-                if self.encoding_type == "utf-8" & isinstance(read_error, UnicodeDecodeError):
-                    print("encoding failed, trying other")
-                    self.encoding_type = "ISO-8859-1"
-                    self.get_data(url)
-                else:
-                    print("encoding failed")
-                    self.flag = True
+                self.get_encoding(file)
+                self.encoding_idx = 0 # reset to default value
+                for line in file:
+                    try:
+                        self.read_line(line)
+                    except Exception as read_error:
+                        print("false encoding")
+                        self.flag_encoding = True
+                        if self.encoding_idx < len(self.encoding) - 1:
+                            self.encoding_idx += 1
+                            self.encoding_final = self.encoding[self.encoding_idx]
+                            self.read_line(line)
+                        else:
+                            print("no matching encoding found. please review")
 
-            for line in file:  # starts at second line
-                try:
-                    decoded_line = line.decode(self.encoding_type)
-                    for quoted_part in re.findall(r'\"(.*?)\"', decoded_line):  # replace 'space' in quotes with '@'
-                        decoded_line = decoded_line.replace(quoted_part, quoted_part.replace(" ", "@"))
-                    split_lines = decoded_line.split(' ')
-                    for i in range(len(split_lines)):
-                        for quoted_part in re.findall(r'\"(.*?)\"', split_lines[i]):  # replace '@' in quotes with 'space'
-                            split_lines[i] = split_lines[i].replace(quoted_part, quoted_part.replace("@", " "))
-                    lines_list.append(split_lines)
-                except Exception as read_error:
-                    if self.encoding_type == "utf-8" & isinstance(read_error, UnicodeDecodeError):
-                        print("encoding failed, trying other")
-                        self.encoding_type = "ISO-8859-1"
-                        self.get_data(url)
-                    else:
-                        print("encoding failed")
-                        self.flag = True
+            except:
+                self.flag_final = True
 
-        return lines_list
+            return self.lines_list
 
     def convert_df(self, data):
 
         df = pd.DataFrame(data)
-        if df.iloc[0,0].find("=") == -1: # first col does not contain data
-            df = df.drop(df.columns[0], axis=1)  # drop first col
+        row, col = df.shape
+        drop_cols = []
+        for i in range(col):
+            if not df.iloc[:,i].str.contains("=").any(): # if col does not contain data
+                drop_cols.append(i)
+        if len(drop_cols) != 0:
+            df = df.drop(drop_cols, axis=1)  # drop respective cols
+
         row, col = df.shape
         col_name_ref = ''  # default value
         col_names = []
 
         for j in range(col):  # get col names and extract values
-
             for i in range(row):
-
                 if i == 0:
-                    col_name_ref = df.iloc[i, j].split("=")[0]
-
+                    col_name_ref = df.iloc[i, j].split("=")[0] # get col name
                 split_element = df.iloc[i, j].split("=")
                 col_name = split_element[0]
 
-                if j == (col - 1):
+                if j == (col - 1): # handling last col
                     value = split_element[1].split("/>")[0][1:-1]
                 else:
                     value = split_element[1][1:-1]
 
-                if col_name_ref != col_name:
-                    print("ERROR: column names do not match")
+                if col_name_ref != col_name: # case of different col names
+                    print("column names do not match. please review error")
                     break
 
-                if value == "":
+                if value == "": # handling missing data
                     df.iloc[i, j] = "NaN"
                 else:
                     df.iloc[i, j] = value
@@ -110,12 +136,11 @@ class txt(object):
 
     def load_data(self, url):
 
-        if self.verify_url(url):
+        if self.verify_url(url) and self.get_file_ending(url) == "txt":
             data = self.get_data(url)
         else:
             print(f"> 3rd-Party Url/Dataset detected and therefore skipped:\n> {url}")
-        print("----")
-        return self.convert_df(data), self.flag
+        return self.convert_df(data), self.flag_final
 
 url = "https://offenedaten-konstanz.de/sites/default/files/FAHRPLAENE.txt"
 #url = "https://offenedaten-konstanz.de/sites/default/files/FAHRTEN.Txt" # dauert ca 1 min
@@ -127,7 +152,7 @@ url = "https://offenedaten-konstanz.de/sites/default/files/FAHRPLAENE.txt"
 #url = "https://offenedaten-konstanz.de/sites/default/files/ORTE.txt"
 #url = "https://offenedaten-konstanz.de/sites/default/files/VERBINDUNGEN.txt"
 
-dsf_txt = txt()
+dsf_txt = txtFetcher()
 result, flag = dsf_txt.load_data(url)
 print(flag)
 print(result)
