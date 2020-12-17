458b64c5a585b99bea93419d7cd7d6b4a0d1dd3f
birkealine <56511466+birkealine@users.noreply.github.com>
2020-12-17 16:40:00 +0100

    Update shp_fetcher.py

--- playground/shp_fetcher.py
+++ playground/shp_fetcher.py
@@ -9,32 +9,41 @@ class SHPFetcher:
     """
 
     def __init__(self):
-        self.seperator = ";"
+        self.flag_final = True
 
-    def fetch_dataset_urls(self, p_id):
-        response = requests.get(PACKAGE_BASE_URL + p_id)  # , header=
-        if response.status_code == 200:
-            return response.json()["result"][0]["resources"][0]["url"]
-        return response.status_code
+#     def fetch_dataset_urls(self, p_id):
+#         response = requests.get(PACKAGE_BASE_URL + p_id)  # , header=
+#         if response.status_code == 200:
+#             return response.json()["result"][0]["resources"][0]["url"]
+#         return response.status_code
 
-    def verify_url(self, url):
-        """
-        check if an url belongs to offene-daten-konstanz.de
-        """
-        return "offenedaten-konstanz.de" in url
+#     def verify_url(self, url):
+#         """
+#         check if an url belongs to offene-daten-konstanz.de
+#         """
+#         return "offenedaten-konstanz.de" in url
 
-    def get_file_ending(self, url):
-        """
-        extract file ending from an url
-        """
-        return url.split(".")[::-1][0]
+#     def get_file_ending(self, url):
+#         """
+#         extract file ending from an url
+#         """
+#         return url.split(".")[::-1][0]
 
-    def load_data(self, p_id):
-        print(f"Loading data for {p_id}")
-        url = self.fetch_dataset_urls(p_id)
-        if self.verify_url(url) and self.get_file_ending(url) == "zip" or self.verify_url(url) or self.get_file_ending(url) == "json":
-            data = gpd.read_file(url)
-            return data
+    def parse_geo(self, url):
+        try: 
+            df = gpd.read_file(url)
+            return df
+        except: 
+            self.flag_final = False
+            return gpd.GeoDataFrame()
+
+    def load_data(self, url):
+        return self.parse_geo(url), self.flag_final
+#         print(f"Loading data for {p_id}")
+#         url = self.fetch_dataset_urls(p_id)
+#         if self.verify_url(url) and self.get_file_ending(url) == "zip" or self.verify_url(url) or self.get_file_ending(url) == "json":
+#             data = gpd.read_file(url)
+#             return data
 #
 # import matplotlib.pyplot as plt
 # tink_id = "c6ed879e-9a2c-41ca-a168-a88d449a24c1"
