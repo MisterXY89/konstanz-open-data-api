1c5494a90db179b4c3726fa47e6160fb300f0803
birkealine <birke.pfeifle@uni-konstanz.de>
2021-01-07 17:49:57 +0100

    added save_data() function

--- playground/API.py
+++ playground/API.py
@@ -1,18 +1,23 @@
 import pandas as pd
 import numpy as np
 import requests
+import urllib.request
+import os
 from config import *
 
-from csv_fetcher import csvFetcher
+from csv_fetcher import CSVFetcher 
 #import shp_fetcher
 #import xls_fetcher
 #from txt_fetcher import txtFetcher
 
 current_list = pd.read_csv(CURRENT_PACKAGE_LIST_FILE)
-#formats = ["csv", "xls", "txt", "shp"]
+
+# list containing all the file endings there are:
+formats = ["csv", "xls", "txt", "zip", "json", "geojson"]
+
 __formats = {
     #"txt": txtFetcher,
-    "csv": csvFetcher
+    "csv": CSVFetcher
 }
 
 def __fetch_dataset_urls(id):
@@ -24,13 +29,16 @@ def __fetch_dataset_urls(id):
     return response.status_code
 
 def __get_url_ending(url):
+    '''
+    helper function to get the file ending of a url
+    '''
     return url.split(".")[::-1][0].lower()
 
-def get_data(data, tag = False, external = False):
-    """
-    input : list containing names or tags (string)
-    return : single df or dict containing df
-    """
+# made a separate function for this because it is needed in both, get_data() and save_data() 
+def __create_id_list(data, tag = False):
+    '''
+    helper function to create a list of ids for the datasets indicated by the names/tags given
+    '''
     id_list = []
     if tag:
         for i in range(len(data)):
@@ -42,6 +50,21 @@ def get_data(data, tag = False, external = False):
             if data[i] in current_list["name"].values:
                 id_element = np.array2string(current_list[current_list['name'] == data[i]]['id'].values)
                 id_list.append(id_element[2:-2])
+    
+    return id_list
+
+def __verify_url(url):
+    """
+    helper function to check if an url belongs to offenedaten-konstanz.de
+    """
+    return "offenedaten-konstanz.de" in url.lower()
+
+def get_data(data, tag = False, external = False):
+    """
+    input : list containing names or tags (string)
+    return : single df or dict containing df
+    """
+    id_list = __create_id_list(data)
 
     result_dict = {}
     final_flag = True
@@ -65,7 +88,49 @@ def get_data(data, tag = False, external = False):
 
     return result
 
-#test = get_data(["Geo"], tag=True)
-#test = get_data(["standorte_glascontainer"])
-test = get_data(["historische_wetterdaten"])
-print(test)
+def save_data(data, tag = False, folder=""): 
+    '''
+    function to save the indicated data (or data fitting the indicated tags) to your local disk
+    
+    INPUT:
+    data: list of Strings 
+        list containing names of the datasets you want to store 
+        or tags for which you want to save the respective datasets
+    tag: Boolean
+        default: False
+        set to True if data list contains tags
+    folder: String
+        default: empty
+        if you wanted to save the data to a different folder than the one from which you are executing the python file,
+        you could indicate the respective folder here (use either forward slashes '/' or double backward slashes '\\')
+    '''
+    #doesn't work for links leading to jsons 
+
+    id_list = __create_id_list(data)
+
+    # create list containing urls: (could possibly be made more compact if you used the __fetch_dataset_urls() function, but not sure how to use it)
+    url_list = []
+    for id in id_list: 
+        response = requests.get(PACKAGE_BASE_URL + id)
+        if response.status_code == 200:
+            resources = response.json()["result"][0]["resources"]
+            for resource in resources:
+                if __get_url_ending(resource["url"]) in formats: # if url leads to a file
+                    url_list.append(resource["url"])
+
+    # save all the files indicated by the urls: 
+    for url in url_list: 
+        file_name = ""
+        if len(folder)>0: # if a specific folder was given:
+            file_name = os.path.join(folder, url.rsplit('/', 1)[1])
+        else: #if no local path is given: save to current working directory
+            file_name = os.path.join(os.getcwd(), url.rsplit('/', 1)[1])
+        urllib.request.urlretrieve (url, file_name) # command to actually save the data
+        print("Finished saving requested data to " + file_name)
+
+# test = get_data(["Geo"], tag=True)
+# test = get_data(["standorte_glascontainer"])
+# test = get_data(["historische_wetterdaten"])
+# print(test)
+save_data(["standorte_sportanlagen"], folder = "C:/Users/bikki/Downloads")
+# save_data(["standorte_sportanlagen"])
