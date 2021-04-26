e5c3db7eff01f044c6264b320e30bac2bc94e801
Tilman Kerl <tilman.kerl@uni-konstanz.de>
2021-04-26 19:12:05 +0200

    mv test and prep

--- opencity/test.py
+++
@@ -1,5 +0,0 @@
-from API import get_data
-
-test = get_data(["Geo"], tag=True)
-kinder = "Kindertagesbetreuung - Einrichtungen csv"
-print(test[kinder])
---
+++ test.py
@@ -0,0 +1,12 @@
+from opencity import config as conf 
+from opencity import opencity as oc
+
+cf = conf.Config(PKG_FOLDER="/home/dragonfly")
+open_city = oc.OpenCity(cf=cf)
+
+
+open_city.show_data()
+open_city.show_data(overview=True)
+
+# res = open_city.get_data("parkplaetze")
+# print(res)
