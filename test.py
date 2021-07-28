dda90e33fafb9a8d859c0b8eaa317e813e841450
Tilman Kerl <tilman.kerl@uni-konstanz.de>
2021-07-28 15:24:54 +0200

    add first tests

--- test.py
+++ test.py
@@ -1,20 +1,78 @@
-from opencity import config as conf 
+
+import os
+import pandas as pd
+
 from opencity import opencity as oc
+from opencity import config as conf 
 
-cf = conf.Config(PKG_FOLDER="/Users/silkehusse/Documents/CorrelAid/konstanz-open-data-api/opencity")
-open_city = oc.OpenCity(cf=cf)
-#open_city = oc.OpenCity()
+# ! run pytest interactive -> some test cases needs user input ! #
 
-#open_city = oc.OpenCity()
+# -------------------- obj init prep -------------------- #
 
-open_city.get_data(["Politik und Wahlen"], tag = True, meta = True)
+# change depending on test env
+ABSOLUTE_DIR = "/home/dragonfly/Documents/CorrelAid/"
 
-#open_city.show_data()
-#open_city.show_data(overview=True)
+# change if file is renamed in future
+PKG_FILENAME = "CURRENT_PACKAGE_LIST.csv"
 
+# helper clean function to catch File does not exist errors
+def clean(obj, file=True):
+    try:
+        if file: 
+            os.remove(obj)
+        else:
+            os.removedirs(obj)
+    except Exception as e:
+        print(e)
+    
+# clean all files potentially laying arround
+clean(PKG_FILENAME)
+clean(f"existing_test_pkg_folder/{PKG_FILENAME}")
+clean(f"test_pkg_folder/{PKG_FILENAME}")
+clean("test_pkg_folder", file=False)
+clean(f"{ABSOLUTE_DIR}{PKG_FILENAME}")
 
-res = open_city.get_data(["oeffentliche_papiereimer"])
-# print(res)
-# print(type(res))
-# res = open_city.save_data(["parkplaetze"], folder="test_folder/")
-# print(res)
+# -------------------- obj init -------------------- #
+
+def test_obj_init_without_config():
+    open_city = oc.OpenCity()
+    assert isinstance(open_city, oc.OpenCity)
+
+def test_obj_init_config():
+    cf = conf.Config(PKG_FOLDER="")
+    assert isinstance(cf, conf.Config)
+    
+def test_obj_init_with_config_relative_folder_non_existing():
+    cf = conf.Config(PKG_FOLDER="test_pkg_folder/")
+    open_city = oc.OpenCity(cf = cf)
+    assert os.path.isfile(cf.CURRENT_PACKAGE_LIST_FILE)
+    
+def test_obj_init_with_config_relative_folder_non_existing_no_trailing_sep():
+    cf = conf.Config(PKG_FOLDER="test_pkg_folder")
+    open_city = oc.OpenCity(cf = cf)
+    assert os.path.isfile(cf.CURRENT_PACKAGE_LIST_FILE)
+    
+def test_obj_init_with_config_relative_folder_existing():
+    cf = conf.Config(PKG_FOLDER="existing_test_pkg_folder/")
+    open_city = oc.OpenCity(cf = cf)
+    assert os.path.isfile(cf.CURRENT_PACKAGE_LIST_FILE)
+
+def test_obj_init_with_config_absolute_folder_existing():
+    cf = conf.Config(PKG_FOLDER=ABSOLUTE_DIR)
+    open_city = oc.OpenCity(cf = cf)
+    assert os.path.isfile(cf.CURRENT_PACKAGE_LIST_FILE)
+    
+# -------------------- save_data -------------------- #
+def test_save_data_no_target_one_source():
+    open_city = oc.OpenCity()
+    files = open_city.save_data(["parkplaetze"], file_ret=True)
+    assert files    
+    for file in files:
+        assert os.path.isfile(file)
+    # assert os.path.isfile("") # 
+    
+    
+    # todo: alle data sets + kombis
+    
+
+    
