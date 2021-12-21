929053153dffea357f28deb0659ae112b3dbe19b
Rahkakavee Baskaran <rahkakavee.baskaran@cause-effect.io>
2021-12-21 20:27:34 +0100

    add links to fetcher

--- opencity/tests/test_fetcher/test_kml_fetcher.py
+++ opencity/tests/test_fetcher/test_kml_fetcher.py
@@ -1,52 +1,48 @@
-# from fetcher.kml_fetcher import kmlFetcher
-# import pytest
-# import pandas
+from fetcher.kml_fetcher import kmlFetcher
+import pytest
+import pandas
 
 
-# @pytest.fixture
-# def fetcher_instance() -> kmlFetcher:
-#     """creates a instance of jsonFetcher
+@pytest.fixture
+def fetcher_instance() -> kmlFetcher:
+    """creates a instance of jsonFetcher
 
-#     Returns
-#     -------
-#     jsonFetcher
-#         instance of jsonFetcher
-#     """
-#     kml = kmlFetcher()
-#     return kml
-
-
-# # def test_load_data_correct(fetcher_instance):
-# #     """checks if data is really a pandas core frame, this tests also parse_kml already
-
-# #     Parameters
-# #     ----------
-# #     fetcher_instance : kmlFetcher
-# #         json kml instance
-# #     """
-# #     df, flag = fetcher_instance.load_data(
-# #         url="https://offenedaten-konstanz.de/api/3/action/package_show?id=d3303376-77d7-4dba-bd6f-c0194acb7ae2"
-# #     )
-# #     assert isinstance(df, pandas.core.frame.DataFrame)
-# #     assert flag == True
-# #     assert len(list(df)) != 0
+    Returns
+    -------
+    jsonFetcher
+        instance of jsonFetcher
+    """
+    kml = kmlFetcher()
+    return kml
 
 
-# # def test_load_data_wrong(fetcher_instance):
-# #     """checks if for a wrong url no dataframe is returned, this tests also parse_json already
+def test_load_data_correct(fetcher_instance):
+    """checks if data is really a pandas core frame, this tests also parse_kml already
 
-# #     Parameters
-# #     ----------
-# #     fetcher_instance : kmlFetcher
-# #         kml fetcher instance
-# #     """
-# #     df, flag = fetcher_instance.load_data(
-# #         url="https://offenedaten-konstanz.de/api/3/action/package_show?id=d3303376-77d7-4dba-bd6f-c0194acb7ae21313"
-# #     )
-# #     # assert not isinstance(df, pandas.core.frame.DataFrame)
-# #     assert flag == False
-# #     assert len(list(df)) == 0
+    Parameters
+    ----------
+    fetcher_instance : kmlFetcher
+        json kml instance
+    """
+    df, flag = fetcher_instance.load_data(
+        url="https://opendata.arcgis.com/datasets/1600ac31789c4779b4613f74e70c1f3f_0.kml"
+    )
+
+    assert flag == True
+    assert len(df) > 0
 
 
-# # TODO: Wo bekommt man wirklich ein json File
-# # TODO: Warum gibt test_load_data_wrong bei json ein pandas_core.DataFrame raus?
+def test_load_data_wrong(fetcher_instance):
+    """checks if for a wrong url no dataframe is returned, this tests also parse_json already
+
+    Parameters
+    ----------
+    fetcher_instance : kmlFetcher
+        kml fetcher instance
+    """
+    df, flag = fetcher_instance.load_data(
+        url="https://offenedaten-konstanz.de/api/3/action/package_show?id=d3303376-77d7-4dba-bd6-c0194acb7ae21313"
+    )
+    # assert not isinstance(df, pandas.core.frame.DataFrame)
+    assert flag == False
+    assert len(df) == 0
--- opencity/tests/test_fetcher/xls_fetcher.py
+++ opencity/tests/test_fetcher/xls_fetcher.py
@@ -1,62 +1,60 @@
-# import pytest
-# from fetcher.shp_fetcher import shpFetcher
-# import pandas
+import pytest
+from fetcher.xls_fetcher import xlsFetcher
+import pandas
 
 
-# @pytest.fixture
-# def fetcher_instance() -> shpFetcher:
-#     """creates a instance of jsonFetcher
+@pytest.fixture
+def fetcher_instance() -> xlsFetcher:
+    """creates a instance of jsonFetcher
 
-#     Returns
-#     -------
-#     jsonFetcher
-#         instance of jsonFetcher
-#     """
-#     shp = shpFetcher()
-#     return shp
-
-
-# def test_verify_df_correct(fetcher_instance):
-#     df, flag_df = fetcher_instance.load_data(
-#         url="https://offenedaten-konstanz.de/api/3/action/package_show?id=2c6ae437-883b-4395-9780-23a16edb6720"
-#     )
-#     flag = fetcher_instance.verify_df(
-#         df1=df,
-#         url="https://offenedaten-konstanz.de/api/3/action/package_show?id=2c6ae437-883b-4395-9780-23a16edb6720",
-#         encoding="utf-8",
-#         sep=";",
-#     )
-#     assert flag == True
-#     assert flag == flag_df
+    Returns
+    -------
+    jsonFetcher
+        instance of jsonFetcher
+    """
+    xls = xlsFetcher()
+    return xls
 
 
-# def test_load_data_correct(fetcher_instance):
-#     """checks if data is really a pandas core frame, this tests also parse_csv already
-
-#     Parameters
-#     ----------
-#     fetcher_instance : csvFetcher
-#         csv fetcher instance
-#     """
-#     df, flag = fetcher_instance.load_data(
-#         url="https://offenedaten-konstanz.de/api/3/action/package_show?id=2c6ae437-883b-4395-9780-23a16edb6720"
-#     )
-#     assert isinstance(df, pandas.core.frame.DataFrame)
-#     assert flag == True
-#     assert len(list(df)) != 0
+def test_verify_df_correct(fetcher_instance):
+    df, flag_df = fetcher_instance.load_data(
+        url="https://offenedaten-konstanz.de/sites/default/files/%C3%96ffentlichen%20M%C3%BClltonnen.xls"
+    )
+    flag = fetcher_instance.verify_df(
+        df1=df,
+        url="https://offenedaten-konstanz.de/sites/default/files/%C3%96ffentlichen%20M%C3%BClltonnen.xls",
+        encoding="utf-8",
+        sep=";",
+    )
+    assert flag == True
+    assert flag == flag_df
 
 
-# def test_load_data_wrong(fetcher_instance):
-#     """checks if for a wrong url no dataframe is returned, this tests also parse_csv already
+def test_load_data_correct(fetcher_instance):
+    """checks if data is really a pandas core frame, this tests also parse_csv already
 
-#     Parameters
-#     ----------
-#     fetcher_instance : csvFetcher
-#         csv fetcher instance
-#     """
-#     df, flag = fetcher_instance.load_data(
-#         url="https://offenedaten-konstanz.de/api/3/action/package_show?id=2c6ae437-883b-4395-9780-23a16edb61231310"
-#     )
-#     assert not isinstance(df, pandas.core.frame.DataFrame)
-#     assert flag == False
-#     assert len(list(df)) == 2
+    Parameters
+    ----------
+    fetcher_instance : csvFetcher
+        csv fetcher instance
+    """
+    df, flag = fetcher_instance.load_data(
+        url="https://offenedaten-konstanz.de/sites/default/files/%C3%96ffentlichen%20M%C3%BClltonnen.xls"
+    )
+    assert len(df) > 0
+    assert flag == True
+
+
+def test_load_data_wrong(fetcher_instance):
+    """checks if for a wrong url no dataframe is returned, this tests also parse_csv already
+
+    Parameters
+    ----------
+    fetcher_instance : csvFetcher
+        csv fetcher instance
+    """
+    df, flag = fetcher_instance.load_data(
+        url="https://offenedaten-konstanz.de/sites/default/files/%C3%96ffentlichen%20M%C3%BClltonen.xls"
+    )
+    assert len(df) == 0
+    assert flag == False
